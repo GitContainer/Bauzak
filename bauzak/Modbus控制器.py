9eca1f886603497bad45f56099a8f37d9934e95f
薛丞宏 <ihcaoe@gmail.com>
2017-03-18 11:09:40 +0800

    一般的32bit試驗加好矣

--- bauzak/Modbus控制器.py
+++ bauzak/Modbus控制器.py
@@ -1,8 +1,10 @@
+from fatek.symbol import Symbol
 from fatek.target import FatekTarget
 from pymodbus3.client.sync import ModbusTcpClient
 
 
 class Modbus控制器:
+    一擺數量限制 = 125
 
     def __init__(self, ip, port):
         self.連線 = ModbusTcpClient(host=ip, port=port)
@@ -19,24 +21,37 @@ class Modbus控制器:
     def 讀32位元的資料(self, 表):
         位置名對應表 = {}
         for 位, 名 in 表.items():
-            位置名對應表[int(位.strip('R'))] = 名
-#             檢查攏愛雙數
-        開始, *_, 結束 = sorted(位置名對應表.keys())
-        位址數量 = (結束 + 2 - 開始)
-# 檢查袂使超過125
-        result = self.連線.read_holding_registers(開始, 位址數量)
-        原始資料 = result.registers
+            位置名對應表[Symbol(位).offset] = 名
 
+        這段開始 = None
+        這段結束 = None
         上尾資料 = {}
-        for 第幾个, (細, 大) in enumerate(zip(原始資料[::2], 原始資料[1::2])):
-            對應位址 = 開始 + 第幾个 + 第幾个
+        for 位置 in sorted(位置名對應表.keys()):
+            這个開始 = 位置
+            這个結束 = 位置 + 2
+            if 這段開始 is None:
+                這段開始 = 這个開始
+                這段結束 = 這个結束
+            elif 這个結束 - 這段開始 <= self.一擺數量限制:
+                這段結束 = 這个結束
+            else:
+                self._讀32位元一段資料(位置名對應表, 上尾資料, 這段開始, 這段結束)
+                這段開始 = 這个開始
+                這段結束 = 這个結束
+        if 這段開始 is not None:
+            self._讀32位元一段資料(位置名對應表, 上尾資料, 這段開始, 這段結束)
+        return 上尾資料
+
+    def _讀32位元一段資料(self, 位置名對應表, 上尾資料, 這段開始, 這段結束):
+        result = self.連線.read_holding_registers(這段開始, 這段結束 - 這段開始)
+        原始資料 = result.registers
+        for 對應位址, (細, 大) in enumerate(zip(原始資料[:-1], 原始資料[1:]), start=這段開始):
             try:
                 名 = 位置名對應表[對應位址]
             except:
                 pass
             else:
                 上尾資料[名] = 細 + 大 * 65536
-        return 上尾資料
 
     def 讀16位元的資料(self, 表):
         位置名對應表 = {}
---
+++ saanu/test讀32位元.py
@@ -0,0 +1,101 @@
+from unittest.case import TestCase
+from unittest.mock import MagicMock
+from bauzak.Modbus控制器 import Modbus控制器
+
+
+class 讀coils(TestCase):
+
+    def setUp(self):
+        self.PLC控制 = Modbus控制器(
+            'localhost', '502'
+        )
+        self.PLC控制.連線 = MagicMock()
+
+    def test_無半个(self):
+        表 = {
+        }
+        self.PLC控制.讀32位元的資料(表)
+        self.PLC控制.連線.read_holding_registers.assert_not_called()
+
+    def test_一个(self):
+        表 = {
+            'R1036': 'A做了幾項',
+        }
+        self.PLC控制.讀32位元的資料(表)
+        self.PLC控制.連線.read_holding_registers.assert_called_once_with(1036, 2)
+
+    def test_讀的表佇仝一段(self):
+        表 = {
+            'R1036': 'A做了幾項',
+            'R1038': 'B做了幾項',
+        }
+        self.PLC控制.讀32位元的資料(表)
+        self.PLC控制.連線.read_holding_registers.assert_called_once_with(1036, 4)
+
+    def test_仝段上遠(self):
+        表 = {
+            'R1000': 'A做了幾項',
+            'R1123': 'B做了幾項',
+        }
+        self.PLC控制.讀32位元的資料(表)
+        self.PLC控制.連線.read_holding_registers.assert_called_once_with(1000, 125)
+
+    def test_讀的表佇無仝一段(self):
+        表 = {
+            'R1000': 'A做了幾項',
+            'R1124': 'B做了幾項',
+        }
+        self.PLC控制.讀32位元的資料(表)
+        self.PLC控制.連線.read_holding_registers.assert_any_call(1000, 2)
+        self.PLC控制.連線.read_holding_registers.assert_any_call(1124, 2)
+
+    def test_無仝的暫存器嘛會使(self):
+        表 = {
+            'R1036': 'A做了幾項',
+            'R1136': 'AA做了幾項',
+            'D100': 'B做了幾項',
+            'D110': 'BB做了幾項',
+        }
+        self.PLC控制.讀32位元的資料(表)
+        self.PLC控制.連線.read_holding_registers.assert_any_call(1036, 102)
+        self.PLC控制.連線.read_holding_registers.assert_any_call(6100, 12)
+
+    def test_暫存器數字(self):
+        表 = {
+            'R1036': 'A做了幾項',
+            'R1038': 'AA做了幾項',
+            'D100': 'B做了幾項',
+            'D104': 'BB做了幾項',
+        }
+        答案 = {
+            'A做了幾項': 10,
+            'AA做了幾項': 20000,
+            'B做了幾項': 65536 * 6 + 3,
+            'BB做了幾項': 65536,
+        }
+        頭一个回應 = MagicMock()
+        頭一个回應.registers = [10, 0, 20000, 0]
+        第二个回應 = MagicMock()
+        第二个回應.registers = [3, 6, 0, 0, 0, 1]
+        self.PLC控制.連線.read_holding_registers.side_effect = [頭一个回應, 第二个回應]
+
+        結果 = self.PLC控制.讀32位元的資料(表)
+        self.assertEqual(結果, 答案)
+
+    def test_連續暫存器(self):
+        表 = {
+            'R1036': 'A做了幾項',
+            'R1037': 'AA做了幾項',
+            'R1038': 'AAA做了幾項',
+        }
+        答案 = {
+            'A做了幾項': 65536 * 2 + 1,
+            'AA做了幾項': 65536 * 3 + 2,
+            'AAA做了幾項': 65536 * 4 + 3,
+        }
+        頭一个回應 = MagicMock()
+        頭一个回應.registers = [1, 2, 3, 4]
+        self.PLC控制.連線.read_holding_registers.return_value = 頭一个回應
+
+        結果 = self.PLC控制.讀32位元的資料(表)
+        self.assertEqual(結果, 答案)
